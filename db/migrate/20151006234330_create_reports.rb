3dd6bfa17f94a1adcbf7b7a991a05927f419a5b5
Metuz <dead.man99@hotmail.com>
2015-10-21 13:25:43 -0500

    Adding model,store and bought_at to report

--- db/migrate/20151006234330_create_reports.rb
+++ db/migrate/20151006234330_create_reports.rb
@@ -6,8 +6,12 @@ class CreateReports < ActiveRecord::Migration
       t.integer :user_id
       t.integer :location_id
       t.string :name
+      t.string :model
+      t.string :store
+      t.datetime :bought_at
       t.string :serial
       t.string :number
+      t.integer :brand_id
 
 
       t.timestamps
